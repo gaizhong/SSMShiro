925ba77749eae9d3c0f0208e1f54287bc2e00583
gaizhong <619039879@qq.com>
2017-11-16 13:51:24 +0800

    添加数据库

--- src/com/ssmshiro/realms/FirstRealms.java
+++ src/com/ssmshiro/realms/FirstRealms.java
@@ -1,12 +1,17 @@
 package com.ssmshiro.realms;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
@@ -16,9 +21,16 @@ import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ssmshiro.dao.UserDao;
+import com.ssmshiro.daoImpl.UserDaoImpl;
+import com.ssmshiro.userService.UserService;
+import com.ssmshiro.userServiceImpl.UserServiceImpl;
 
 public class FirstRealms extends AuthorizingRealm {
-
+	@Autowired
+	UserServiceImpl userservice;
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
 		System.out.println("[FirstRealm] doGetAuthenticationInfo");
@@ -26,6 +38,8 @@ public class FirstRealms extends AuthorizingRealm {
 		String username=uptoken.getUsername();
 		String password=String.valueOf(uptoken.getPassword());
 		Object principal=username;
+		List list = userservice.queryUser(null);
+		System.out.println(list);
 		//fc1709d0a95a6be30bc5926fdb7f22f4   123456经过 MD5加密后1024次后
 //		Object credentials="123456";
 		Object credentials="fc1709d0a95a6be30bc5926fdb7f22f4";
@@ -34,6 +48,10 @@ public class FirstRealms extends AuthorizingRealm {
 			credentials="038bdaf98f2037b31f1e75b5b4c9b26e";
 		}else if("user".equals(username)){
 			credentials="098d2c478e9c11555ce2823231e02ec1";
+		}
+		//4. 若用户不存在, 则可以抛出 UnknownAccountException 异常
+		if("unknown".equals(username)){
+			throw new UnknownAccountException("用户不存在!");
 		}
 		ByteSource credentialsSalt=ByteSource.Util.bytes(username);
 		SimpleAuthenticationInfo info=new 
